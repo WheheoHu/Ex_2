f193058a8025e0b9beb3d0ce57149fd135c57e33
文昊 胡 <hwh19981124@hotmail.com>
2019-03-22 09:12:35 +0800

    menufunc.h

---
+++ Ex_2/MenuFunc.h
@@ -0,0 +1,149 @@
+#pragma once
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "glut.h"
+#include "CoordinateXY.h"
+
+using namespace std;
+
+#define WHITE 0
+#define RED 1
+#define GREEN 2
+#define	BLUE 3
+
+static int iColor = WHITE;
+static CoordinateXY   coorxy;
+vector< CoordinateXY> datapoint;
+fstream datafile;
+
+//初始化菜单
+void InitMenu();
+//右键菜单更改颜色
+void processmenu(int MenuID);
+//保存数据
+void savedata(vector<CoordinateXY> datapoint);
+//读取Data.txt数据
+void loaddata(fstream &datafile);
+//清屏&重置数据
+void removeall(fstream &datafile);
+
+
+void InitMenu()
+{
+	int m_menuID = glutCreateMenu(processmenu);
+	glutSetMenu(m_menuID);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutAddMenuEntry("White", 0);
+	glutAddMenuEntry("Red", 1);
+	glutAddMenuEntry("Green", 2);
+	glutAddMenuEntry("Blue", 3);
+	glutAddMenuEntry("SAVE", 4);
+	glutAddMenuEntry("LOAD", 5);
+	glutAddMenuEntry("CLEAN ALL!", 6);
+	glutAddMenuEntry("EXIT", 7);
+}
+void processmenu(int MenuID) {
+
+	switch (MenuID)
+	{
+	case 0: {
+		iColor = WHITE;
+		glColor3f(1, 1, 1);
+		break;
+	}
+	case 1: {
+		iColor = RED;
+		glColor3f(1, 0, 0);
+		break;
+	}
+	case 2: {
+		iColor = GREEN;
+		glColor3f(0, 1, 0);
+		break;
+	}
+	case 3: {
+		iColor = BLUE;
+		glColor3f(0, 0, 1);
+		break;
+	}
+	case 4: {
+		savedata(datapoint);
+		break;
+	}
+	case 5: {
+		loaddata(datafile);
+		break;
+	}
+	case 6: {
+
+		removeall(datafile);
+		break;
+	}
+	case 7: {
+		exit(0);
+		break; }
+	}
+}
+
+
+void savedata(vector<CoordinateXY> datapoint)
+{
+	//remove("Data.txt");
+	datafile.open("Data.txt", ios::out | ios::app);
+	for (vector<CoordinateXY>::iterator iter = datapoint.begin(); iter != datapoint.end(); iter++)
+	{
+		datafile << iter->getCoorX() << " " << iter->getCoorY() << " " << iter->getColor() << endl;
+	}
+	datafile.close();
+}
+
+void loaddata(fstream & datafile)
+{
+	datafile.open("Data.txt", ios::in);
+	int coorx;
+	int coory;
+	int pointcolor;
+
+	while (!datafile.eof())
+	{
+		datafile >> coorx;
+		datafile >> coory;
+		datafile >> pointcolor;
+		iColor = pointcolor;
+		switch (iColor)
+		{
+		case RED: {
+			glColor3f(1, 0, 0);
+			break;
+		}
+		case GREEN: {
+			glColor3f(0, 1, 0);
+			break;
+		}
+		case BLUE: {
+			glColor3f(0, 0, 1);
+			break;
+		}
+		case WHITE: {
+			glColor3f(1, 1, 1);
+			break;
+		}
+		}
+		glBegin(GL_POINTS);
+		glVertex2i(coorx, coory);
+		glEnd();
+		glutPostRedisplay();
+	}
+	datafile.close();
+}
+
+void removeall(fstream & datafile)
+{
+	datafile.open("Data.txt", ios::out);
+	datafile.close();
+	loaddata(datafile);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
--- Ex_2/MyFunc.h
+++ Ex_2/MyFunc.h
@@ -7,6 +7,7 @@
 #include <string>
 #include "glut.h"
 #include "CoordinateXY.h"
+#include  "MenuFunc.h"
 
 using namespace std;
 
@@ -18,7 +19,7 @@ using namespace std;
 #define	BLUE 3
 
 bool creak = false;
-bool saved = false;
+
 static int inc = 0;
 static int iColor = WHITE;
 static CoordinateXY   coorxy;
@@ -27,15 +28,8 @@ fstream datafile;
 
 //保存xy数据
 void setXY(int x, int y);
-void processmenu(int MenuID);
-//保存数据
-void savedata(vector<CoordinateXY> datapoint);
-//读取Data.txt数据
-void loaddata(fstream &datafile);
-//清屏&重置数据
-void removeall(fstream &datafile);
-//初始化菜单
-void InitMenu();
+
+
 //初始化窗口
 void InitWindow();
 //渲染
@@ -43,79 +37,9 @@ void RenderScene();
 //鼠标操作，将坐标传递给cooxy
 void mouseProcess(int button, int state, int x, int y);
 
-void savedata(vector<CoordinateXY> datapoint)
-{
-	//remove("Data.txt");
-	datafile.open("Data.txt", ios::out | ios::app);
-	for (vector<CoordinateXY>::iterator iter = datapoint.begin(); iter != datapoint.end(); iter++)
-	{
-		datafile << iter->getCoorX() << " " << iter->getCoorY() << " " << iter->getColor() << endl;
-	}
-	datafile.close();
-}
 
-void loaddata(fstream & datafile)
-{
-	datafile.open("Data.txt", ios::in);
-	int coorx;
-	int coory;
-	int pointcolor;
 
-	while (!datafile.eof())
-	{
-		datafile >> coorx;
-		datafile >> coory;
-		datafile >> pointcolor;
-		iColor = pointcolor;
-		switch (iColor)
-		{
-		case RED: {
-			glColor3f(1, 0, 0);
-			break;
-		}
-		case GREEN: {
-			glColor3f(0, 1, 0);
-			break;
-		}
-		case BLUE: {
-			glColor3f(0, 0, 1);
-			break;
-		}
-		case WHITE: {
-			glColor3f(1, 1, 1);
-			break;
-		}
-		}
-		glBegin(GL_POINTS);
-		glVertex2i(coorx, coory);
-		glEnd();
-		glutPostRedisplay();
-	}
-	datafile.close();
-}
 
-void removeall(fstream & datafile)
-{
-	datafile.open("Data.txt", ios::out);
-	datafile.close();
-	loaddata(datafile);
-	glClear(GL_COLOR_BUFFER_BIT);
-}
-
-void InitMenu()
-{
-	int m_menuID = glutCreateMenu(processmenu);
-	glutSetMenu(m_menuID);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-	glutAddMenuEntry("White", 0);
-	glutAddMenuEntry("Red", 1);
-	glutAddMenuEntry("Green", 2);
-	glutAddMenuEntry("Blue", 3);
-	glutAddMenuEntry("SAVE", 4);
-	glutAddMenuEntry("LOAD", 5);
-	glutAddMenuEntry("CLEAN ALL!", 6);
-	glutAddMenuEntry("EXIT", 7);
-}
 
 void InitWindow()
 {
@@ -160,50 +84,5 @@ void setXY(int x, int y) {
 	coorxy.setColor(iColor);
 	coorxy.plusnum();
 	datapoint.push_back(CoordinateXY(x, y, iColor));
-	//datafile << x << " " << y << " " << iColor << endl;
-}
-
-//右键菜单更改颜色
-void processmenu(int MenuID) {
-
-	switch (MenuID)
-	{
-	case 0: {
-		iColor = WHITE;
-		glColor3f(1, 1, 1);
-		break;
-	}
-	case 1: {
-		iColor = RED;
-		glColor3f(1, 0, 0);
-		break;
-	}
-	case 2: {
-		iColor = GREEN;
-		glColor3f(0, 1, 0);
-		break;
-	}
-	case 3: {
-		iColor = BLUE;
-		glColor3f(0, 0, 1);
-		break;
-	}
-	case 4: {
-		savedata(datapoint);
-		break;
-	}
-	case 5: {
-		loaddata(datafile);
-		break;
-	}
-	case 6: {
-
-		removeall(datafile);
-		break;
-	}
-	case 7: {
-		exit(0);
-		break; }
-	}
 }
 
